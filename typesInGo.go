217006a7b08f2e80fe1beb4ffd1ef722661cda5f
Alfreddk <alfredwungdk@gmail.com>
2022-03-28 16:37:06 +0800

    added var and print types

--- typesInGo.go
+++ typesInGo.go
@@ -1,9 +1,15 @@
 package main
 
+import "fmt"
+
 //Insert variables declarations here based on activity
 
 func tellMeTypes() {
 	//insert code here to print out types of variables
+	text := "The following is the account information."
+	firstName := "Luke"
+
+	fmt.Printf("%T %T", text, firstName)
 }
 
 func main() {
